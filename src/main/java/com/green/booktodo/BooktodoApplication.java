1bd69f6125da2ee1aa1445c40d51059b1aed137c
김다율 <kdy12061004@gmail.com>
2023-06-21 09:58:50 +0900

    독서 투두리스트 생성

---
+++ src/main/java/com/green/booktodo/BooktodoApplication.java
@@ -0,0 +1,13 @@
+package com.green.booktodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooktodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/booktodo/BooktodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.booktodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
